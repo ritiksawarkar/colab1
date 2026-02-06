5d02f5bea615029966086b1ef7d53f35400a2132
ritiksawarkar <ritiksawarkar027@gmail.com>
2026-02-06 22:54:56 +0530

    changes in app.jsx

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
